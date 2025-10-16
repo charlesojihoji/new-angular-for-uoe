cfc2c4fe23c79f114e62e00f88bce2adece2948e
charlesojihoji <charlesojihoji@gmail.com>
2025-10-16 11:52:10 -0400

    Added admin modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { FacultyComponent } from './pages/faculty/faculty.component';
-import { CoursesComponent } from './pages/courses/courses.component';
+import { HomeComponent } from './front/pages/home/home.component';
+import { AboutComponent } from './front/pages/about/about.component';
+import { ContactComponent } from './front/pages/contact/contact.component';
+import { FacultyComponent } from './front/pages/faculty/faculty.component';
+import { CoursesComponent } from './front/pages/courses/courses.component';
+import { FrontComponent } from './front/front.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'contact', component: ContactComponent},
-    {path: 'courses', component: CoursesComponent},
-    {path: 'faculty', component: FacultyComponent}
+    {path: '', component: FrontComponent, children: [
+        {path: '', component: HomeComponent},
+        {path: 'about', component: AboutComponent},
+        {path: 'contact', component: ContactComponent},
+        {path: 'courses', component: CoursesComponent},
+        {path: 'faculty', component: FacultyComponent}
+    ]},
+
+    {path: 'admin', component: AdminComponent},
+
+    {path: 'user', component: UserComponent}
+    
 ];
